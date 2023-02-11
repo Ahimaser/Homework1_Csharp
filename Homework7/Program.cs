05caeca61f8bcd1fb81e8201bc9ee749cd845c07
Евгений <geniy15049999@gmail.com>
2023-02-11 13:48:32 +0300

    add homework7

---
+++ Homework7/Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+
+/*Задача 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+m = 3, n = 4.
+0,5 7 -2 -0,2
+1 -3,3 8 -9,9
+8 7,8 -7,1 9*/
+Console.WriteLine("Задача № 1 \n");
+Console.Write("Введите m: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите n: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+double[,] array = new double[m, n];
+
+for (int i = 0; i < array.GetLength(0); i++)
+{
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        array[i, j] = new Random().NextDouble() * 10;
+        Console.Write($"{Math.Round((array[i, j]), 1)} ");
+    }
+    Console.WriteLine();
+}
+
+
+/*Задача 50.Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, и возвращает значение этого элемента или же указание, что такого элемента нет.
+
+Например, задан массив:
+
+1 4 7 2
+
+5 9 2 3
+
+8 4 2 4
+
+17->такого числа в массиве нет*/
+Console.WriteLine("\nЗадача № 2 \n");
+Console.Write("Введите позицию элемента в двумерном массиве: ");
+int target = Convert.ToInt32(Console.ReadLine());
+
+int temp1 = 0;
+int temp2 = 10;
+for (int i = 0; i < array.GetLength(0); i++)
+{
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        array[i, j] = new Random().Next(1, 10);
+        Console.Write($"{array[i, j]} ");
+    }
+    Console.WriteLine();
+}
+for (int i = 0; i < array.GetLength(0); i++)
+{
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        if (target == temp2 + temp1)
+        {
+            Console.WriteLine($"{target} -> {array[i, j]}");
+            break;
+        }
+        temp1++;
+    }
+    temp1 = 0;
+    temp2 = temp2 + 10;
+    Console.WriteLine();
+}
+
+/*Задача 52.Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.*/
+Console.WriteLine("\nЗадача № 3 \n");
+for (int i = 0; i < array.GetLength(0); i++)
+{
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        array[i, j] = new Random().Next(1, 10);
+        Console.Write($"{array[i, j]} ");
+    }
+    Console.WriteLine();
+}
+double temp = 0;
+Console.Write("Среднее арифмитическое каждого столбца -> ");
+for (int i = 0; i < array.GetLength(0); i++)
+{
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        temp = (temp + array[i, j]);
+    }
+    Console.Write($"{Convert.ToDouble(temp) / n}; ");
+    temp = 0;
+}
---
+++ prectice7/Program.cs
@@ -0,0 +1,130 @@
+﻿using System;
+
+int x1 = Convert.ToInt32(Console.ReadLine());
+int x2 = Convert.ToInt32(Console.ReadLine());
+/*Задача 46: Задайте двумерный массив размером m×n, заполненный случайными целыми числами.
+m = 3, n = 4.
+1 4 8 19
+5 -2 33 -2
+77 3 8 1*/
+void DoubleArray( int m, int n)
+{
+    int[,] arr = new int[m, n];
+    for(int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            arr[i, j] = new Random().Next(0, 100);
+            Console.Write(arr[i, j]);
+            Console.Write(" ");
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+    Console.WriteLine("-----");
+}
+
+
+
+
+/*Задача 48: Задайте двумерный массив размера m на n, каждый элемент в массиве находится по формуле: Aₘₙ = m + n.Выведите полученный массив на экран.
+m = 3, n = 4.
+ 0 1 2 3
+1 2 3 4
+2 3 4 5*/
+void DoubleArray2(int m, int n)
+{
+    int[,] arr = new int[m, n];
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            arr[i, j] = i + j;
+            Console.Write(arr[i, j]);
+            Console.Write(" ");
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+    Console.WriteLine("-----");
+}
+
+/*
+Задача 49: Задайте двумерный массив. Найдите элементы, у которых оба индекса чётные, и замените эти элементы на их квадраты.
+Например, изначально массив
+ выглядел вот так:
+1 4 7 2
+5 9 2 3
+8 4 2 4*/
+void ChangeArray(int m, int n)
+{
+    Console.WriteLine("Изначальный массив: ");
+    int[,] arr = new int[m, n];
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            arr[i, j] = i + j;
+            Console.Write(arr[i, j]);
+            Console.Write(" ");
+           
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+    Console.WriteLine("Измененный массив: ");
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            if(i % 2 == 0 && j%2==0)
+            {
+                arr[i, j] = arr[i, j] * arr[i, j];
+               
+            }
+            Console.Write(arr[i, j]);
+            Console.Write(" ");
+
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+    Console.WriteLine("-----");
+}
+
+
+
+/*Задача 51: Задайте двумерный массив. Найдите сумму элементов, находящихся на главной диагонали (с индексами (0,0); (1; 1) и т.д.
+ Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+Сумма элементов главной диагонали: 1+9+2 = 12*/
+void SummArray(int m,int n)
+{
+    int[,] arr = new int[m, n];
+    int summ = 0;
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            arr[i, j] = i + j;
+            Console.Write(arr[i, j]);
+            Console.Write(" ");
+            if (i == j)
+            {
+                summ += arr[i, j];
+            }
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine($"Сумма центральных элементов = {summ}");
+}
+DoubleArray(x1, x2);
+DoubleArray2(x1, x2);
+ChangeArray(x1, x2);
+SummArray(x2, x1);
+
+/*x1 ^ 5 + x2 ^ 5 + x3 ^ 5 + x4 ^ 5 = x5 ^ 5
+Сумма 4 чисел в 5 степени равна пятому числу в пятой степени
+Нужно найти эти числа (их 5) (от 1 до 300)*/
